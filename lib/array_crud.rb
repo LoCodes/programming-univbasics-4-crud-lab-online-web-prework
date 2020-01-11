f5ef96734d1a6ad81917ed1599c8beb75fd367f4
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-11 01:54:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,8 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  remove_element_from_start_of_array
-  remove_element_from_start_of_array
+  remove_element_from_start_of_array = ["I", "am", "really", "learning", "arrays!"]
+  array[0] = remove_element_from_start_of_array
   
 end
 
